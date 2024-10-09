ad83008752543dfbc2bd06663af133c6e708675a
abyan <abyan.huq@outlook.com>
2024-10-08 23:38:06 -0500

    Assignmnt-6 done

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
         OrganCompatibilityAnalyzer inventory = new OrganCompatibilityAnalyzer();
 
-        // Add sample organs
         inventory.addOrgan(new Organ("O1", "A+", 300, "1-2-3-4-5-6"));
         inventory.addOrgan(new Organ("O2", "O-", 280, "1-3-5-7-9-11"));
 
-        // Add sample patients
         inventory.addPatient(new Patient("P1", "A+", 70, "1-2-3-4-5-6"));
         inventory.addPatient(new Patient("P2", "B-", 80, "2-4-6-8-10-12"));
         inventory.addPatient(new Patient("P3", "O+", 65, "1-3-5-7-9-11"));
@@ -17,7 +15,7 @@ public class Main {
         int[][] compatibilityMatrix = inventory.createCompatibilityMatrix();
         inventory.displayMatrix(compatibilityMatrix);
 
-        double[] weights = {0.4, 0.3, 0.3}; // Blood type, Weight, HLA
+        double[] weights = {0.4, 0.3, 0.3};
         inventory.displayWeightMatrix(weights);
 
         double[][] weightedMatrix = inventory.calculateWeightedCompatibility(weights);
